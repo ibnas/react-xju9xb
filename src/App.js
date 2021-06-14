aa659e2935fe09a93d059ce4fb356074d8f3a1a5
ibnas <ibnasir00@gmail.com>
2021-06-14 19:57:31 +0100

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './style.css';
 import Button from '@material-ui/core/Button';
 import Database from './database';
+import DocsView from './docsView.js';
+import DocsList from './docsList.js';
 
 export default function App() {
-  return <Database />;
+  return (
+    <>
+      {/* <Database /> */}
+      {/* <DocsView></DocsView> */}
+      <DocsList />
+    </>
+  );
 }
 //
---
+++ src/docsView.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './style.css';
+import { Grid, Paper, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Database from './database';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(1)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+}));
+
+export default function DocsView(props) {
+  const classes = useStyles();
+
+  let docs = props.docs
+    ? props.docs
+    : [
+        { type: 'file', data: { name: 'file.f', content: 'content' } },
+        { type: 'file', data: { name: 'file.f', content: 'content' } },
+        { type: 'file', data: { name: 'file.f', content: 'content' } },
+        { type: 'file', data: { name: 'file.f', content: 'content' } },
+        { type: 'file', data: { name: 'file.f', content: 'content' } },
+        {
+          type: 'folder',
+          data: {
+            name: 'folder',
+            content: {
+              type: 'file',
+              data: { name: 'file.f', content: 'content' }
+            }
+          }
+        }
+      ];
+  return (
+    <>
+      <Box
+        border={1}
+        borderColor="blue"
+        borderRadius="5px"
+        bgcolor="#e6f1ff"
+        className={classes.root}
+      >
+        <Grid container spacing={3}>
+          {docs.map(doc => {
+            return (
+              <Grid item>
+                <Paper className={classes.paper}>{doc.data.name}</Paper>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
+    </>
+  );
+}
+//
