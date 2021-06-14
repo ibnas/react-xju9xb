9824300c2d3ef5a2f422854cd77b8c0c6006b4eb
ibnas <ibnasir00@gmail.com>
2021-06-14 18:46:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './style.css';
+import Button from '@material-ui/core/Button';
+import Database from './database';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-    </div>
+   <Database></Database>
   );
 }
