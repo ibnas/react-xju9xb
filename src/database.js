3e837dd7d83da417698cf7799e4fd0454ea5872b
ibnas <ibnasir00@gmail.com>
2021-06-14 18:46:18 +0100

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Container, Button, Box } from '@material-ui/core';
 
-export default function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch'
+    }
+  },
+  collapse: {
+    overflow: 'hidden'
+  },
+  collapseContent: {
+    display: props => {
+      return props.open ? 'hidden' : 'block';
+    }
+  }
+}));
+
+export default function Database() {
+  const classes = useStyles();
+  let [open, setOpen] = useState(true);
+
   return (
-    <div>
-      <input></input>
-    </div>
+    <Box className={classes.collapse}>
+      <Box
+        className={classes.collapseContent}
+        border={1}
+        borderColor="blue"
+        borderRadius="5px"
+        bgcolor="#f2f5f9"
+      >
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="standard-basic" label="server" variant="outlined" />
+          <TextField id="filled-basic" label="user" variant="outlined" />
+          <TextField id="outlined-basic" label="password" variant="outlined" />
+          <Button variant="contained" color="primary">
+            Set
+          </Button>
+        </form>
+      </Box>
+      <span onClick={() => setOpen(!open)}>{open ? 'open' : 'close'}</span>
+    </Box>
   );
 }
+
