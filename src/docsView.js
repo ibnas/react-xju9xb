1821baefcb914f3bac2e6f8b4b4054c6f94446b6
ibnas <ibnasir00@gmail.com>
2021-06-17 02:19:02 +0100

    2 files modified

--- src/docsView.js
+++ src/docsView.js
@@ -50,7 +50,7 @@ export default function DocsView(props) {
           className={classes.root}
         >
           <Grid container spacing={3}>
-            {docs.map(doc => {
+            {docs.map((doc, index) => {
               return (
                 <Grid item>
                   <Select>
@@ -60,6 +60,7 @@ export default function DocsView(props) {
                       // }}
                       variant="outlined"
                       className={classes.paper}
+                      tabindex={index}
                     >
                       {doc.data.name}
                     </Paper>
@@ -86,12 +87,47 @@ let Select = props => {
     };
   };
 
+  // document.addEventListener(
+  //   'keydown',
+  //   event => {
+  //     const keyName = event.key;
+
+  //     if (keyName === 'Control') {
+  //       // do not alert when only Control key is pressed.
+  //       return;
+  //     }
+
+  //     if (event.ctrlKey) {
+  //       // Even though event.key is not 'Control' (e.g., 'a' is pressed),
+  //       // event.ctrlKey may be true if Ctrl key is pressed at the same time.
+  //       alert(`Combination of ctrlKey + ${keyName}`);
+  //     } else {
+  //       alert(`Key pressed ${keyName}`);
+  //     }
+  //   },
+  //   false
+  // );
+
+  // document.addEventListener(
+  //   'keyup',
+  //   event => {
+  //     const keyName = event.key;
+
+  //     // As the user releases the Ctrl key, the key is no longer active,
+  //     // so event.ctrlKey is false.
+  //     if (keyName === 'Control') {
+  //       alert('Control key was released');
+  //     }
+  //   },
+  //   false
+  // );
   return (
     <ClickAwayListener onClickAway={() => setSelected(false)}>
       <div
         onMouseEnter={() => setMouseIn(true)}
         onMouseLeave={() => setMouseIn(false)}
         onClick={() => setSelected(true)}
+        onKeyPress={e => console.log(e.key + ' key')}
         style={style()}
       >
         {props.children}
